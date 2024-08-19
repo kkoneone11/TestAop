ee7207fd4fa8a6d3c100fd560ec80373b4ed4242
zhengyi.huang <huangzhengyi.hzy@alibaba-inc.com>
2024-08-19 19:53:00 +0800

    修改

--- src/main/java/com/kkoneone/aop/TestAop.java
+++ src/main/java/com/kkoneone/aop/TestAop.java
@@ -12,6 +12,16 @@ import org.springframework.context.annotation.Configuration;
 @Configurable
 public class TestAop {
 
+//    @Around
+//    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+//        System.out.println("环绕通知前置拦截器");
+//        final Object ret = joinPoint.invoke();
+//        System.out.println("环绕通知后置拦截器");
+//        //对应切点的方法
+//        return ret;
+//
+//    }
+
     @Before
     public void before(JoinPoint joinPoint){
         Plus plus = joinPoint.getAnnotation(Plus.class);
@@ -19,23 +29,15 @@ public class TestAop {
         System.out.println("before");
     }
 
-    @Throwing
-    public void throwing(){
-        System.out.println("出异常了.....");
-    }
-    @After
-    public void after(JoinPoint joinPoint){
-        final Object returnValue = joinPoint.getArgs();
-        System.out.println("after");
-    }
+//    @Throwing
+//    public void throwing(){
+//        System.out.println("出异常了.....");
+//    }
+//    @After
+//    public void after(JoinPoint joinPoint){
+//        final Object returnValue = joinPoint.getArgs();
+//        System.out.println("after");
+//    }
 
-    @Around
-    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("环绕通知前置拦截器");
-        final Object ret = joinPoint.invoke();
-        System.out.println("环绕通知后置拦截器");
-        //对应切点的方法
-            return ret;
 
-    }
 }
